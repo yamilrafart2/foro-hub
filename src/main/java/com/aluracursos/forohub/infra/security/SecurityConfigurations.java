3d0b9fd9e03c3b495cc4cbd82bc1a1bcdc094bfe
yamilrafart2 <yamilrafart2>
2026-03-06 20:19:06 -0300

    feat: agrega repositorios JPA y configuracion inicial de seguridad
    - Crea interfaces JpaRepository para Usuario, Curso y Topico.
    - Configura SecurityConfigurations para permitir peticiones temporalmente.
    - Prepara la infraestructura base para la persistencia de datos.
    
    	nuevos archivos: src/main/java/com/aluracursos/forohub/domain/curso/CursoRepository.java
    	nuevos archivos: src/main/java/com/aluracursos/forohub/domain/topico/TopicoRepository.java
    	nuevos archivos: src/main/java/com/aluracursos/forohub/domain/usuario/UsuarioRepository.java
    	nuevos archivos: src/main/java/com/aluracursos/forohub/infra/security/SecurityConfigurations.java

---
+++ src/main/java/com/aluracursos/forohub/infra/security/SecurityConfigurations.java
@@ -0,0 +1,20 @@
+package com.aluracursos.forohub.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // <-- Permite todo temporalmente
+                .build();
+    }
+
+}
