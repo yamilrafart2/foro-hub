ffc15c760a88c6330e44f02d26aca380c1c8f66a
yamilrafart2 <yamilrafart2>
2026-03-06 20:20:25 -0300

    feat: implementa endpoint POST para registro de topicos
    - Crea TopicoController con el método registrarTopico.
    - Utiliza @Transactional para asegurar la persistencia.
    - Implementa respuesta 201 Created con URI dinámica y cuerpo de detalle.
    
    	nuevos archivos: src/main/java/com/aluracursos/forohub/controller/TopicoController.java

---
+++ src/main/java/com/aluracursos/forohub/controller/TopicoController.java
@@ -0,0 +1,52 @@
+package com.aluracursos.forohub.controller;
+
+import com.aluracursos.forohub.domain.curso.Curso;
+import com.aluracursos.forohub.domain.curso.CursoRepository;
+import com.aluracursos.forohub.domain.topico.DatosDetalleTopico;
+import com.aluracursos.forohub.domain.topico.DatosRegistroTopico;
+import com.aluracursos.forohub.domain.topico.Topico;
+import com.aluracursos.forohub.domain.topico.TopicoRepository;
+import com.aluracursos.forohub.domain.usuario.Usuario;
+import com.aluracursos.forohub.domain.usuario.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicoController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity registrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico, UriComponentsBuilder uriBuilder) {
+
+        // Obtener las referencias del usuario y el curso desde la base de datos
+        Usuario autor = usuarioRepository.getReferenceById(datosRegistroTopico.autorId());
+        Curso curos = cursoRepository.getReferenceById(datosRegistroTopico.cursoId());
+
+        // Crear y guardar el nuevo tópico
+        Topico topico = new Topico(datosRegistroTopico, autor, curos);
+        topicoRepository.save(topico);
+
+        var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DatosDetalleTopico(topico));
+
+    }
+
+}
